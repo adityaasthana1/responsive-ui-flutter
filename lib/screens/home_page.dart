42a1ad07cb5d24f0e0e75517b9674f6ee047401b
Aditya Asthana <asthana.aditya1@gmail.com>
2022-02-11 15:01:58 +0530

    responsive feed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -18,55 +18,146 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
 
-    return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            if (!Responsive.isDesktop(context))
-              SliverAppBar(
-                elevation: 10,
-                backgroundColor: Colors.white,
-                title: Text(
-                  "ResponsiveX",
-                  style: faceBookText,
-                ),
-                centerTitle: false,
-                floating: true,
-                actions: const [
-                  AppBarIconButton(iconData: Icons.search),
-                  AppBarIconButton(iconData: MdiIcons.facebookMessenger)
-                ],
-              ),
-            SliverToBoxAdapter(
-              child: CreatePostContainer(currentUser: currentUser),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-              sliver: SliverToBoxAdapter(
-                child: RoomsWidget(onlineUsers: onlineUsers),
-              ),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-              sliver: SliverToBoxAdapter(
-                child: StoryBoard(
-                  currentUser: currentUser,
-                  stories: stories,
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return PostCard(post: posts[index]);
-                },
-                childCount: posts.length,
-              ),
-            )
-          ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Responsive(
+            mobile: _HomePageMobile(),
+            tablet: _HomePageMobile(),
+            desktop: _HomePageDesktop(),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _HomePageMobile extends StatelessWidget {
+  const _HomePageMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        if (!Responsive.isDesktop(context))
+          SliverAppBar(
+            elevation: 10,
+            backgroundColor: Colors.white,
+            title: Text(
+              "ResponsiveX",
+              style: faceBookText,
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: const [
+              AppBarIconButton(iconData: Icons.search),
+              AppBarIconButton(iconData: MdiIcons.facebookMessenger)
+            ],
+          ),
+        SliverToBoxAdapter(
+          child: CreatePostContainer(currentUser: currentUser),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: RoomsWidget(onlineUsers: onlineUsers),
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: StoryBoard(
+              currentUser: currentUser,
+              stories: stories,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return PostCard(post: posts[index]);
+            },
+            childCount: posts.length,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _HomePageDesktop extends StatelessWidget {
+  const _HomePageDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.orange,
+          ),
+        ),
+        Spacer(),
+        Container(
+          width: 600,
+          child: CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [
+              if (!Responsive.isDesktop(context))
+                SliverAppBar(
+                  elevation: 10,
+                  backgroundColor: Colors.white,
+                  title: Text(
+                    "ResponsiveX",
+                    style: faceBookText,
+                  ),
+                  centerTitle: false,
+                  floating: true,
+                  actions: const [
+                    AppBarIconButton(iconData: Icons.search),
+                    AppBarIconButton(iconData: MdiIcons.facebookMessenger)
+                  ],
+                ),
+              SliverToBoxAdapter(
+                child: CreatePostContainer(currentUser: currentUser),
+              ),
+              SliverPadding(
+                padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                sliver: SliverToBoxAdapter(
+                  child: RoomsWidget(onlineUsers: onlineUsers),
+                ),
+              ),
+              SliverPadding(
+                padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                sliver: SliverToBoxAdapter(
+                  child: StoryBoard(
+                    currentUser: currentUser,
+                    stories: stories,
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return PostCard(post: posts[index]);
+                  },
+                  childCount: posts.length,
+                ),
+              )
+            ],
+          ),
+        ),
+        Spacer(),
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.blue,
+          ),
+        )
+      ],
     );
   }
 }
